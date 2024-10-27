9a33064cb4218de6cd4d5fbe91da18231d037a1e
Molina-Q <quentinmolina1@gmail.com>
2024-10-27 16:35:28 +0100

    render geometry

--- index.js
+++ index.js
@@ -15,3 +15,21 @@ const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 camera.position.z = 2;
 
 const scene = new THREE.Scene();
+
+
+const geo = new THREE.IcosahedronGeometry(1.0, 2);
+const mat = new THREE.MeshBasicMaterial({ 
+    color: 0x00ff00, 
+    wireframe: true
+});
+const mesh = new THREE.Mesh(geo, mat);
+scene.add(mesh);
+
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+
+animate();
+
+
